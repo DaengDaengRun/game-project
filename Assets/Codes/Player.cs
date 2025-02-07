9a2e262aec598d591046cb5c27a706f73a13cfcc
Hyewon Yoon <iey704@gachon.ac.kr>
2025-02-07 20:51:55 +0900

    feat: enemy 관련 파일 추가

--- Assets/Codes/Player.cs
+++ Assets/Codes/Player.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.SearchService;
 using UnityEngine;
-using System;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
