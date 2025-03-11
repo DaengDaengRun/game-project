bc7cd5052021e17e326d2b33dcd6a64cdf1e3b5b
AyunJeong <ayun01j07@gmail.com>
2025-03-12 01:22:40 +0900

    feat: 뼈다구 안 찾은 경우 경고패널 구현

--- Assets/Codes/Player.cs
+++ Assets/Codes/Player.cs
@@ -22,6 +22,8 @@ public class Player : MonoBehaviour
     public Sprite sickDogSprite;    // 적과 충돌 시 나타나는 캐릭터
     public Sprite happyDogSprite;    // 집 찾으면 나타나는 캐릭터
     public Sprite findDogSprite;    // 뼈다귀 찾으면 나타나는 캐릭터
+    public GameObject GetBoneWarning;
+    public float warningDisplayTime = 0.1f;
 
     void Awake(){
         if (instance == null)
@@ -112,7 +114,22 @@ void OnCollisionEnter2D(Collision2D collision){
         }
         else {
             Debug.Log("🍖❌ 뼈다귀를 찾아오세요!");
+            ShowWarningMessage();
         }
+    }
+
+    void ShowWarningMessage()
+    {
+        Debug.Log("⚠️ ShowWarningMessage 호출됨");
+        GetBoneWarning.SetActive(true);    // 패널 보여주기
+        StartCoroutine(HideWarningAfterTime());
+    }
+
+    IEnumerator HideWarningAfterTime()
+    {
+        yield return new WaitForSeconds(warningDisplayTime);
+        Debug.Log("👉 HideWarningMessage 호출됨 (Coroutine)");
+        GetBoneWarning.SetActive(false);
     }
 }
 
